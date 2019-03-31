38967cd3f1349887eb94ceceb543505dcadeaa1c
Karthikeyan Selvarajan <ks33@illinois.edu>
2019-03-31 06:03:38 -0700

    removed unused import

--- src/main/java/com/claims/repository/ClaimsRepository.java
+++ src/main/java/com/claims/repository/ClaimsRepository.java
@@ -3,7 +3,6 @@ package com.claims.repository;
 import com.claims.model.Claims;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.Collection;
